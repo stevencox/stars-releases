de1ece4738010b6eedaeb9ea9612c4bf32d53138
evryscope <evryscope@stars-dw4.edc.renci.org>
2017-09-27 16:48:26 -0400

    run chronos

--- bin/chronos.sh
+++ bin/chronos.sh
@@ -5,11 +5,13 @@ set -e
 
 cd /opt
 
-if [ ! -d chronos ]; then
+if [ -d chronos ]; then
+    cd chronos
+    git pull
+else
     git clone git@github.com:stevencox/stars-releases.git chronos
+    cd chronos
 fi
-
-cd chronos
 
 export MESOS_NATIVE_^CBRARY=/usr/local/lib/libmesos.so
 /bin/java -cp chronos*.jar org.apache.mesos.chronos.scheduler.Main \
