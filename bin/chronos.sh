5e8339d7a9a33f8520f03a7e2736e9afaffbec92
evryscope <evryscope@stars-dw4.edc.renci.org>
2017-09-27 16:39:04 -0400

    run chronos

--- bin/chronos.sh
+++ bin/chronos.sh
@@ -9,6 +9,8 @@ if [ ! -d chronos ]; then
     git clone git@github.com:stevencox/stars-releases.git chronos
 fi
 
+cd chronos
+
 /bin/java -cp chronos*.jar org.apache.mesos.chronos.scheduler.Main \
     --zk_hosts=zk://stars-dc0.edc.renci.org:2181,stars-dc1.edc.renci.org:2181,stars-dc2.edc.renci.org:2181 \
     --master=zk://stars-dc0.edc.renci.org:5050,stars-dc1.edc.renci.org:5050,stars-dc2.edc.renci.org:5050/mesos
